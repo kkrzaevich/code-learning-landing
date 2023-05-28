fd30c47634e1739cdf72acc368065251c3f8ee5c
Konstantin Korolev <konstantinrzaevich@gmail.com>
2023-05-28 16:33:55 +0300

    added styles for text and colors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import css from "./style.css";
 import cssReset from "./css-reset.css";
 import cssVariables from "./css-variables.css";
+import textStyles from "./text-styles.css";
+import colorStyles from "./bg-color-styles.css";
+import margins from "./paddings-margins.css";
 import { doSomething } from "./scripts/something";
 
 doSomething();
